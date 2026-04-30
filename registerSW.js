b4e0488791e45227df37307e59d6c14ef77466a4
Cat <cataddcat@gmail.com>
2026-04-30 19:21:49 +0700

    +

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/g/sw.js', { scope: '/g/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/c/sw.js', { scope: '/c/' })})}
